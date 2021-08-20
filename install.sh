ed620d5cdb928bc7f8f34d31069401234d603f63
Grigoriy Safronov <gvsafronov@yandex.ru>
2021-08-20 14:27:04 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -56,6 +56,7 @@ make
 cp /root/fluidb/fluexe/src/fluexe.so /root/fluidb/src/modules
 		   
 		   cd /root && cd fluidb && cd src
+rm keydb-sentinel		   
            
 ./fluidb-serv /root/fluidb/fluidb.conf
 
